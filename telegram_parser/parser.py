828bd021d8dd95f889825fe24ea13dd2dad9a4c7
dvnua <166237167+dvnua@users.noreply.github.com>
2026-09-02 11:13:59 +0300

    parser.py

---
+++ telegram_parser/parser.py
@@ -0,0 +1,11 @@
+import os
+import time
+
+print("========================================")
+print(" Telegram Channel Parser")
+print(" Python работает")
+print(" Telethon будет подключен следующим этапом")
+print("========================================")
+
+while True:
+    time.sleep(60)
